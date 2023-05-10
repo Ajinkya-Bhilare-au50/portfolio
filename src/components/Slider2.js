1345ab8f52ec5b8c5996ae3a135a8fa792772099
Ajinkya Bhilare <ajinkya0011@gmail.com>
2023-05-10 16:36:56 +0530

    updates

--- src/components/Slider2.js
+++ src/components/Slider2.js
@@ -1,4 +1,3 @@
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,7 +8,21 @@ import "swiper/css/navigation";
 import image1 from "../images/image1.jpg";
 
 // import required modules
-import { Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+// Define custom styles for the Swiper slider
+const styles = {
+  container: {
+    backgroundColor: "blue",
+  },
+  slide: {
+    backgroundColor: "red",
+  },
+  arrow: {
+    color: "white",
+    textShadow: "1px 2px 9px #F4AAB9",
+  },
+};
 
 export default function App() {
   return (
@@ -17,38 +30,53 @@ export default function App() {
       <Swiper
         pagination={{
           type: "progressbar",
+          progressBarColor: "green",
         }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="w-1/2"
+        slidesPerView={1}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        speed={1000}
+        loop={true}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="w-1/2 m-14"
+        style={styles.container}
       >
-        <SwiperSlide>
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide style={styles.slide}>
           <img className="w-full" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
+        <SwiperSlide style={styles.slide}>
+          <img className="w-full sm:w-full object-cover" src={image1} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="w-full" src={image1} alt="" />
-        </SwiperSlide>
+
+        <div className="swiper-button-next" style={styles.arrow}></div>
+        <div className="swiper-button-prev" style={styles.arrow}></div>
       </Swiper>
     </>
   );
