5496c6907fcd14bf1d9d85e4a3bdf568b45a96c0
Ajinkya Bhilare <ajinkya0011@gmail.com>
2023-05-10 13:19:09 +0530

    updates

--- src/components/Slider2.js
+++ src/components/Slider2.js
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import image1 from "../images/image3.jpg";
+import image1 from "../images/image1.jpg";
 
 // import required modules
 import { Pagination, Navigation } from "swiper";
