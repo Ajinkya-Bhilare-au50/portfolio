ce3ea200103ac0a49a8b2bbc7236bc51d1a52a74
Ajinkya Bhilare <107457872+Ajinkya-Bhilare-au50@users.noreply.github.com>
2024-06-02 12:07:16 +0530

    Update ResumeButton.js

--- src/components/ResumeButton.js
+++ src/components/ResumeButton.js
@@ -1,19 +1,42 @@
+// import React from "react";
+// import { BsDownload } from "react-icons/bs";
+// const ResumeButton = () => {
+//   const handleDownload = () => {
+//     const resumeUrl =
+//       "https://drive.google.com/uc?export=download&id=1Ce3KZWfuDI5-SqmjqqDabxQ4mv3jiuHb";
+
+//     window.location.href = resumeUrl;
+//   };
+
+//   return (
+//     <button
+//       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex gap-1 items-center"
+//       onClick={handleDownload}
+//     >
+//       <BsDownload /> Download Resume
+//     </button>
+//   );
+// };
+
+// export default ResumeButton;
+
 import React from "react";
 import { BsDownload } from "react-icons/bs";
-const ResumeButton = () => {
-  const handleDownload = () => {
-    const resumeUrl =
-      "https://drive.google.com/uc?export=download&id=1Ce3KZWfuDI5-SqmjqqDabxQ4mv3jiuHb";
 
-    window.location.href = resumeUrl;
+const ResumeButton = () => {
+  const handleOpen = () => {
+    const resumeUrl =
+      "https://drive.google.com/file/d/1Ce3KZWfuDI5-SqmjqqDabxQ4mv3jiuHb/view?usp=sharing";
+
+    window.open(resumeUrl, "_blank");
   };
 
   return (
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex gap-1 items-center"
-      onClick={handleDownload}
+      onClick={handleOpen}
     >
-      <BsDownload /> Download Resume
+      <BsDownload /> View Resume
     </button>
   );
 };
